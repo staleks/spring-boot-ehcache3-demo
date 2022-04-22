7cc5902d19864f1e740c11b80e032b40e08c5042
Aleksandar <staleks@gmail.com>
2022-04-22 19:36:28 +0200

    Added Prometheus in order to monitor cache statistics

---
+++ documentation/links.md
@@ -0,0 +1,4 @@
+# Usefull links
+
+- [JCacheConfig#1](https://github.com/NorthDragons/targ_soft/blob/c90049542a684f2a497716d847ce909898bc8bb3/targ_soft/src/main/java/by/targ_soft/exercise/config/CacheConfig.java)
+- [JCacheConfig#2](https://github.com/vaibsgharge/my-household-appliances-app/blob/735f25cfbd920312ddc7e478e155220364b63c94/src/main/java/com/household/appliances/config/CacheConfiguration.java)
--- src/main/java/com/example/demo/config/cache/CustomPostCreatedCacheEventListener.java
+++ src/main/java/com/example/demo/config/cache/CustomPostCreatedCacheEventListener.java
@@ -5,15 +5,42 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.cache.event.CacheEntryCreatedListener;
 import javax.cache.event.CacheEntryEvent;
+import javax.cache.event.CacheEntryExpiredListener;
 import javax.cache.event.CacheEntryListenerException;
+import javax.cache.event.CacheEntryRemovedListener;
+import javax.cache.event.CacheEntryUpdatedListener;
 
 @Slf4j
-public class CustomPostCreatedCacheEventListener implements CacheEntryCreatedListener<Long, Post> {
+public class CustomPostCreatedCacheEventListener implements CacheEntryCreatedListener<Long, Post>,
+        CacheEntryUpdatedListener<Long, Post>,
+        CacheEntryExpiredListener<Long, Post>,
+        CacheEntryRemovedListener<Long, Post> {
 
     @Override
     public void onCreated(final Iterable<CacheEntryEvent<? extends Long, ? extends Post>> cacheEntryEvents) throws CacheEntryListenerException {
         for (CacheEntryEvent entryEvent: cacheEntryEvents) {
-            log.info("Cached [key: {}, value: {}]", entryEvent.getKey(), entryEvent.getValue());
+            log.info("Cached [key: {}, value: {}]. Type: {}", entryEvent.getKey(), entryEvent.getValue(), entryEvent.getEventType());
+        }
+    }
+
+    @Override
+    public void onExpired(final Iterable<CacheEntryEvent<? extends Long, ? extends Post>> cacheEntryEvents) throws CacheEntryListenerException {
+        for (CacheEntryEvent entryEvent: cacheEntryEvents) {
+            log.info("Expired: [key: {}, value: {}]. Type: {}", entryEvent.getKey(), entryEvent.getValue(), entryEvent.getEventType());
+        }
+    }
+
+    @Override
+    public void onRemoved(final Iterable<CacheEntryEvent<? extends Long, ? extends Post>> cacheEntryEvents) throws CacheEntryListenerException {
+        for (CacheEntryEvent entryEvent: cacheEntryEvents) {
+            log.info("Removed: [key: {}, value: {}]. Type: {}", entryEvent.getKey(), entryEvent.getValue(), entryEvent.getEventType());
+        }
+    }
+
+    @Override
+    public void onUpdated(final Iterable<CacheEntryEvent<? extends Long, ? extends Post>> cacheEntryEvents) throws CacheEntryListenerException {
+        for (CacheEntryEvent entryEvent: cacheEntryEvents) {
+            log.info("Updated: [key: {}, value: {}]. Type: {}", entryEvent.getKey(), entryEvent.getValue(), entryEvent.getEventType());
         }
     }
 }
